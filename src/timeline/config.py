f16c20606780c132456c9ea27795b6a28720e20d
Philipp <p.pichler1@gmail.com>
2020-11-13 12:43:11 +0100

    initial commit

---
+++ src/timeline/config.py
@@ -0,0 +1,73 @@
+GEO_SETTING = dict(
+	scope='europe',
+	landcolor="#c1c1c1",
+	countrycolor="white",
+	projection_type='mercator',
+	showsubunits=True,
+	lonaxis_range=[9.3, 17.5],
+	lataxis_range=[46, 49.5],
+
+)
+
+SLIDERS_DICT = {
+	"active": 0,
+	"yanchor": "top",
+	"xanchor": "left",
+	"currentvalue": {
+		"font": {"size": 20},
+		"visible": True,
+		"xanchor": "right"
+	},
+	"transition": {"duration": 300, "easing": "cubic-in-out"},
+	"pad": {"b": 10, "t": 50},
+	"len": 0.9,
+	"x": 0.1,
+	"y": 0,
+	"steps": []
+}
+
+BUTTON_MENU_DICT = {
+	"buttons": [
+		{
+			"args": [None, {
+				"frame": {"duration": 500, "redraw": True},
+				"fromcurrent": True, "transition": {
+					"duration": 300,
+					"easing": "quadratic-in-out"}
+			}],
+			"label": "Play",
+			"method": "animate"
+		},
+		{
+			"args": [[None], {
+				"frame": {"duration": 0, "redraw": True},
+				"mode": "immediate",
+				"transition": {"duration": 0}
+			}],
+			"label": "Pause",
+			"method": "animate"
+		}
+	],
+	"direction": "left",
+	"pad": {"r": 10, "t": 87},
+	"showactive": False,
+	"type": "buttons",
+	"x": 0.1,
+	"xanchor": "right",
+	"y": 0,
+	"yanchor": "top"
+}
+
+
+def get_slider_dict(step, txt):
+	return {"args": [
+		[step],
+		{
+			"frame": {"duration": 300, "redraw": True},
+			"mode": "immediate",
+			"transition": {"duration": 300}
+		}
+	],
+		"label": txt,
+		"method": "animate"
+	}
---
+++ src/timeline/timeline.py
@@ -0,0 +1,152 @@
+import csv
+import datetime
+import json
+from collections import OrderedDict
+from dataclasses import dataclass
+from typing import List, Dict
+
+import plotly.express as px
+import plotly.graph_objects as go
+import requests
+
+from timeline.config import GEO_SETTING, BUTTON_MENU_DICT, get_slider_dict, SLIDERS_DICT
+
+df = px.data.gapminder()
+
+from api.graph.utils import SimpleCountriesType
+
+
+# class TimeLineGKZ(object):
+# 	0 Time: str
+# 	1 Bezirk: str
+# 	2 GKZ: str
+# 	3 AnzEinwohner: str
+# 	4 AnzahlFaelle: str
+# 	5 AnzahlFaelleSum: str
+# 	6 AnzahlFaelle7Tage: str
+# 	7 SiebenTageInzidenzFaelle: str
+# 	8 AnzahlTotTaeglich: str
+# 	9 AnzahlTotSum: str
+# 	10 AnzahlGeheiltTaeglich: str
+# 	11 AnzahlGeheiltSum: str
+
+@dataclass
+class SimpleCountyRow(object):
+	time: datetime.datetime
+	county: str
+	population: int
+	cases: int
+	gkz: int
+	ort2: str = None
+	ort3: str = None
+	plz: str = None
+	lat: str = None
+	lng: str = None
+
+
+def get_county_data():
+	URL = 'https://covid19-dashboard.ages.at/data/CovidFaelle_Timeline_GKZ.csv'
+	text = requests.get(URL).content.decode('utf-8')
+	lines = text.splitlines()
+
+	lines.pop(0)
+
+	simple_counties = []
+
+	with open('magic-gkz-map.json', 'r') as reader:
+		gkz_dict = json.loads(reader.read())
+
+	reader = csv.reader(lines, delimiter=';', )
+	for row in reader:
+		time = datetime.datetime.strptime(row[0].split(' ')[0], '%d.%m.%Y')
+		bezirk = row[1]
+		population = row[3]
+		AnzahlFaelle = int(row[4])
+		gkz = row[2]
+
+		if gkz == '900':
+			gkz = '901'  # vienna special case
+
+		simple_county = SimpleCountyRow(time, bezirk, population, AnzahlFaelle, gkz)
+		gkz, ort2, ort3, plz, lat, lng = gkz_dict[simple_county.gkz]
+		simple_county.ort2 = ort2
+		simple_county.ort3 = ort3
+		simple_county.plz = plz
+		simple_county.lat = lat
+		simple_county.lng = lng
+
+		if not lat or not lng:
+			raise ('No lat or lng for:', simple_county)
+
+		simple_counties.append(simple_county)
+
+	return simple_counties
+
+
+import pandas as pd
+
+
+def bubble_map_infections(simple_countries: SimpleCountriesType):
+	simple_counties: List[SimpleCountyRow] = get_county_data()
+
+	grouped = OrderedDict()
+	for item in simple_counties:
+		if item.time not in grouped:
+			grouped[item.time] = [item]
+		else:
+			grouped[item.time].append(item)
+
+	# write_1(simple_counties)
+	write_3(grouped)
+
+
+def write_3(grouped: Dict):
+	frames = []
+	for date, simple_counties in grouped.items():
+		df = pd.DataFrame({
+			'date_str': [str(item.time) for item in simple_counties],
+			'date': [item.time for item in simple_counties],
+			'lat': [item.lat for item in simple_counties],
+			'lon': [item.lng for item in simple_counties],
+			'value': [item.cases for item in simple_counties]
+		})
+		absolute = sum(df['value'])
+		date_str = date.strftime('%d.%m.%Y')
+		frame = go.Frame(
+			layout=dict(title=f' {date_str}, total: {absolute}'),
+			name=date_str,
+			data=[go.Scattergeo(
+				locationmode='USA-states',
+				lon=df['lon'],
+				lat=df['lat'],
+				text=df['value'],
+
+				marker=dict(
+					size=df['value'],
+					autocolorscale=False,
+					line_color='rgb(10,10,10)',
+					line_width=0.5,
+					# cmin=0,
+					# cmax=1000,
+					sizemode='area',
+					colorscale='ylorrd',  # https://plotly.com/python/builtin-colorscales/
+					color=df['value'],
+					opacity=0.7
+				)
+			)])
+		frames.append(frame)
+		SLIDERS_DICT['steps'].append(get_slider_dict(date_str, txt=f' {date_str}, total: {absolute}'))
+
+	fig = go.Figure(
+		data=frames[-1].data[0],
+		frames=frames
+	)
+
+	fig.update_layout(
+		title=f'Timeline der Coronazahlen nach Bezirk',
+		updatemenus=[BUTTON_MENU_DICT],
+		geo=GEO_SETTING,
+		sliders=[SLIDERS_DICT]
+	)
+
+	fig.show()
